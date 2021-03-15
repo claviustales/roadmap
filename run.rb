04e897d700af9a1558a3312470aab023826a5361
Clavius Tales <tales@fortestecnologia.com.br>
2021-03-15 11:53:13 -0300

    atualizacao da entrada de dados

--- run.rb
+++ run.rb
@@ -4,29 +4,31 @@ require_relative 'roadmap'
 
 puts ''
 puts Roadmap.new(
-  starting_at: '2021-03-31',
+  starting_at: '2021-02-24',
   weekly_speed: 8,
   demands: {
+    'Rais 2021' => 0,
+    'Conversão via eSocial Downloads' => 40,
     'Conferência de Totalizadores' =>	10,
     'SST S1.0' =>	40,
-    'Complemento de 13' =>	20,
-    '[A] Leiaute S1.0 eSocial' =>	60,
+    'Controle de Empregados horistas' => 30,
+    'Estagiário remunerado' => 10,
+    'Controle de férias a vencer' =>	6,
+    'Leiaute S1.0 eSocial' =>	60,
+    'Complem. de 13' =>	20,
+    'Outros Vinculos para todas as categorias' =>	20,
+    'Diversos Comple. de Rescisão' =>	20,
+    'Vinculos Concomitantes' =>	20,
+    'Cál. IRRF em folha de adiantamento.' =>	20,
     'Leiaute S2.0 eSocial' =>	60,
-    '[A] Outros Vinculos para todas as categorias' =>	20,
-    '[A] Listagem de férias a vencer' =>	6,
-    '[A] Diversos Comple. de Rescisão' =>	20,
-    '[A] Vinculos Concomitantes' =>	20,
-    '[A] Cál. IRRF em folha de adiant.' =>	20,
-    'Controle de Horistas' => 30,
-    'Estagiário Remunerado' => 10,
-    'Contabilização de Pró-Labore' => 4,
-    'Contabilização de Rescisão' => 10,
-    'Contabilização de Autônomo (RPA)' => 4,
-    'Contabilização de Adiantamento de folha' => 6,
-    'Contabilização de Parcela final de 13º salário' => 10,
-    'Contabilização de Adiantamento de 13º salário' => 6,
-    'Termo de resp. e ficha de SF' => 4,
+    'Contabil. do Pró-Labore (*)' => 4,
+    'Contabil. de Rescisão (*)' => 10,
+    'Contabil. de Autônomos (RPA) (*)' => 4,
+    'Contabil. de Adiant. de folha (*)' => 6,
+    'Contabil. de Parcela Inicial 13 (*)' => 6,
+    'Contabil. de Parcela Final 13 (*)' => 10,
+    'Termo de resp. e ficha do salário-família' => 4,
     'Processos Judiciais' => 30
   }
 )
-puts ''
+puts '', '', '(*) dependência da equipe Fisco', ''
